e629b95168a376443647a3474ee14a18b7f86f35
RizalFR76 <93635876+RizalFR76@users.noreply.github.com>
2021-11-13 15:51:45 +0800

    update

--- 13Nov_mutilasi_tampilan/layouts/footer.php
+++ 13Nov_mutilasi_tampilan/layouts/footer.php
@@ -1,7 +1,7 @@
     <footer id="footer">
         <div class="container">
-        <h3>Selecao</h3>
-        <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+        <h3>Complete Photography</h3>
+        <p>Menyediakan peralatan fotografi</p>
         <div class="social-links">
             <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
             <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
@@ -10,10 +10,10 @@
             <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
         <div class="copyright">
-            &copy; Copyright <strong><span>Selecao</span></strong>. All Rights Reserved
+            Complete <strong><span>Photography</span></strong>
         </div>
         <div class="credits">
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            
         </div>
         </div>
     </footer>
