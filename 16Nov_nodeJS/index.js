df8bbb4827f406cf60914c825a49912c2194da4b
RizalFR76 <93635876+RizalFR76@users.noreply.github.com>
2021-11-16 08:43:52 +0800

    belajar NodeJS

---
+++ 16Nov_nodeJS/index.js
@@ -0,0 +1,33 @@
+// penjumlahan
+function add(a, b) {
+	var hasil = a + b;
+	return hasil;
+}
+const hasil1 = add(7, 3);
+console.log("penjumlahan");
+console.log(hasil1);
+
+// pengurangan
+function pengurangan(a, b) {
+	return a - b;
+}
+const hasil2 = pengurangan(7, 3);
+console.log("pengurangan");
+console.log(hasil2);
+
+// perkalian
+function perkalian(a, b) {
+	return a * b;
+}
+const hasil3 = perkalian(7, 3);
+console.log("perkalian");
+console.log(hasil3);
+
+// pembagian
+function pembagian(a, b) {
+	var hasil = a / b;
+	return hasil;
+}
+const hasil4 = pembagian(7, 3);
+console.log("pembagian");
+console.log(hasil4);
