6099420ffeb33a463c3382b7cfa582291416a198
Rizfrr <93635876+RizalFR76@users.noreply.github.com>
2021-11-17 05:57:32 +0700

    menu

---
+++ a_Project_CI/app/Database/Migrations/2021-11-16-222817_Menu.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Menu extends Migration
+{
+    public function up()
+    {
+        //// Membuat kolom/field untuk tabel menu
+		$this->forge->addField([
+			'id'          => [
+				'type'           => 'INT',
+				'constraint'     => 5,
+				'unsigned'       => true,
+				'auto_increment' => true
+			],
+			'nama'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => '255'
+			],
+			'jenis'      => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 100,
+			],
+			'harga' => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 100,
+			],
+			'penyajian'      => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 100,
+			],
+		]);
+
+		// Membuat primary key
+		$this->forge->addKey('id', TRUE);
+
+		// Membuat tabel menu
+		$this->forge->createTable('menu', TRUE);
+    }
+
+    public function down()
+    {
+        //// menghapus tabel menu
+		$this->forge->dropTable('menu');
+    }
+}
